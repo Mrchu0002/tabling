a88b9ceaa64f4de09112db4ef78b0d0e0b65494e
YRN <118609415+Kascald@users.noreply.github.com>
2024-03-28 17:09:06 +0900

    UserVO , UserInfoVO add...

---
+++ src/main/java/com/tabling/user/member/dto/memberVO.java
@@ -0,0 +1,11 @@
+package com.tabling.user.member.dto;
+
+
+import lombok.Data;
+
+@Data
+public class memberVO {
+	private String userID;  // varchar(30) not null,
+	private String userPwd;  // varchar(255) not null,
+	private String userUUID;  // varchar(50),
+}
